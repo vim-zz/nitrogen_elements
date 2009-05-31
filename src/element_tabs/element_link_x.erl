4fefd20c9285c808090dbd89988d52a712a02507
Andreas Stenius <git@astekk.se>
2009-05-31 08:14:32 +0200

    Adapt elements to new lib structure.

---
+++ src/element_tabs/element_link_x.erl
@@ -0,0 +1,30 @@
+% Nitrogen Web Framework for Erlang
+% Copyright (c) 2008-2009 Rusty Klophaus
+% See MIT-LICENSE for licensing information.
+
+%% This element is only needed until the title attribute is supported by nitrogens #link
+
+-module (element_link_x).
+-include ("nitrogen_elements.hrl").
+-compile(export_all).
+
+reflect() -> record_info(fields, link).
+
+render(ControlID, Record) -> 
+	case Record#link_x.postback of
+		undefined -> ok;
+		Postback -> wf:wire(ControlID, #event { type=click, postback=Postback })
+	end,
+	
+	Content = [
+		wf:html_encode(Record#link_x.text, Record#link_x.html_encode),
+		wf:render(Record#link_x.body)
+	],
+	
+	wf_tags:emit_tag(a, Content, [
+		{id, ControlID},
+		{href, Record#link_x.url},
+		{class, [link, Record#link_x.class]},
+		{style, Record#link_x.style},
+		{title, wf:html_encode(Record#link_x.title, Record#link_x.html_encode)}
+	]).
